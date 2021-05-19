97f9d28bff045d22fa097b464f71ddbe0b86c0a7
amit-chhatbar <chhatbar.amit@gmail.com>
2021-05-19 18:37:18 -0500

    big fix

--- s3.tf
+++ s3.tf
@@ -4,13 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
+  }
 }
-
 provider "aws" {
     profile = "default"
     region  = "us-east-2"
 }
 
-resource "s3_bucket" "s3_friendly_name" {
+resource "aws_s3_bucket" "s3_friendly_name" {
     bucket = "s3-tf-test-amit-05192021"
+    acl    = "private"
 }
