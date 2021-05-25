15fdbc5b7d28a12079fa9f1d19bb07065b706c44
amit-chhatbar <chhatbar.amit@gmail.com>
2021-05-25 13:24:02 -0500

    eip association  and scale up

--- prod/prod.tf
+++ prod/prod.tf
@@ -39,6 +39,8 @@ resource "aws_security_group" "prod_web" {
 
 resource "aws_instance" "prod_web" {
    # ami  = "ami-0915bcb5fa77e4892"
+    count =2
+
     ami           = "ami-012c7314a6f9b09d6"
     instance_type = "t2.nano"
 
@@ -52,9 +54,12 @@ resource "aws_instance" "prod_web" {
     }
 }
 
-resource "aws_eip" "prod_web" {
-    instance = aws_instance.prod_web.id
+resource "aws_eip_association" "prod_web" {
+    instance_id    = aws_instance.prod_web.0.id
+    allocation_id  = aws_eip.prod_web.id
+}
 
+resource "aws_eip" "prod_web" {
     tags = {
         "Terraform" : "true"
     }
