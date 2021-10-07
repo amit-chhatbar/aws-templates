c4e303bd35ec1d8611b8e9afca3020aa326c62b1
amit-chhatbar <chhatbar.amit@gmail.com>
2021-10-07 10:00:50 -0500

    adding backend s3

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,6 +3,15 @@ provider "aws" {
     region  = "us-east-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "tfstat-bucket"
+    key    = "terraform-state/"
+    region = "us-east-2"
+    encrypt = true
+  }
+}
+
 resource "aws_s3_bucket" "tf_test1" {
     bucket = "s3-tf-test-amit-100321"
     acl    = "private"
